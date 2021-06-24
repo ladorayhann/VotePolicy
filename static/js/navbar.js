b6c069e558bc441d1b2be9413f05e2b62ad5ac92
hanifanggawi <hanifanggawi45@gmail.com>
2021-06-24 11:36:29 +0700

    Updated navbar

---
+++ static/js/navbar.js
@@ -0,0 +1,4 @@
+// Onclick handler for toggling navbar
+document.getElementById("dropdown-burger").addEventListener('click', () => {
+    document.getElementById("nav-group").classList.toggle("show")
+})
