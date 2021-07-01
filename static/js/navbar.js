153dfba1e5135805b0f261234352206a0fce9bd9
hanifanggawi <hanifanggawi45@gmail.com>
2021-07-01 07:38:26 +0700

    Added login check on navbar

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,4 +1,22 @@
-// Onclick handler for toggling navbar
+// Onclick handler for toggling navbar buttons
 document.getElementById("dropdown-burger").addEventListener('click', () => {
     document.getElementById("nav-group").classList.toggle("show")
 })
+
+const dropdownClick = (e) => {
+    e.currentTarget.nextElementSibling.classList.toggle("show")
+}
+
+const dropwdownFocusOut = (e) => {
+    if(e.relatedTarget !== null && e.relatedTarget.className === 'dropdown-item'){
+        e.preventDefault();
+    } else {
+        e.currentTarget.nextElementSibling.classList.remove("show")
+    }
+}
+
+const dropdowns = document.getElementsByClassName("nav-dropdown")
+Array.from(dropdowns).forEach(element => {
+    element.addEventListener('click', (e) => dropdownClick(e))
+    element.addEventListener('focusout', (e) => dropwdownFocusOut(e))
+});
