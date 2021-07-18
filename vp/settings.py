68b332f761434e4e6e8d6e4fef0f35527c457d2c
ladorayhann <46314336+ladorayhann@users.noreply.github.com>
2021-07-18 15:56:11 +0700

    Update settings.py

--- vp/settings.py
+++ vp/settings.py
@@ -91,7 +91,7 @@ else:
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'votepolicy',
         'USER': 'postgres',
         'PASSWORD': DB_PASSWORD,
