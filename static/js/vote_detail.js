5eba995698b19fc1021c9808b20be87c555902ae
hanifanggawi <hanifanggawi45@gmail.com>
2021-08-06 09:51:32 +0700

    Added jump to vote button

---
+++ static/js/vote_detail.js
@@ -0,0 +1,25 @@
+document.addEventListener( 'DOMContentLoaded', function () {
+	new Splide( '#card-slider', {
+		perPage    : 3,
+		perMove : 1,
+		width: '70em',
+		padding: {
+			left : '1.5em',
+			right: '1.5em',
+		},
+		gap:'1em',
+		classes: {
+			arrows: 'splide__arrows',
+			arrow : '.carousel-button',
+			prev  : 'splide__arrow--prev carousel-button left',
+			next  : 'splide__arrow--next carousel-button right',
+		},
+		pagination: false,
+		breakpoints: {
+			850: {
+				perPage: 1,
+				width : '35em'
+			}
+		}
+	} ).mount();
+} );
